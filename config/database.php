f22825ea54e84680cd91c6a2994d9fdee3411cb8
Nilton Morais <niltonmorais123@gmail.com>
2015-12-29 19:11:49 -0300

    Mudando config do banco de dados

--- config/database.php
+++ config/database.php
@@ -26,8 +26,8 @@ return [
     |
     */
 
-    //'default' => env('DB_CONNECTION', 'mysql'),
-    'default' => "sqlite",
+    'default' => env('DB_CONNECTION', 'sqlite'),
+    //'default' => "sqlite",
 
     /*
     |--------------------------------------------------------------------------
