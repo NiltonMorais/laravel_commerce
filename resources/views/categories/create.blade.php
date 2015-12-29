cd6a644ddb02ef1e00ae25717a165253c49f3dab
Nilton Morais <niltonmorais123@gmail.com>
2015-12-29 16:16:53 -0300

    categories form model building

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -11,18 +11,11 @@
     @endif
 
     {!! Form::open(['route'=>'admin.categories.store']) !!}
-    <div class="form-group">
-        {!! Form::label('name', 'Name:') !!}
-        {!! Form::text('name', null, ['class'=>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('description', 'Description:') !!}
-        {!! Form::textarea('description', null, ['class'=>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::submit('Add Category', ['class'=>'btn btn-primary ']) !!}
-        <a href="{{ route('admin.categories.index') }}" class='btn btn-default '>Back</a>
-    </div>
+        @include('categories._form')
+        <div class="form-group">
+            {!! Form::submit('Add Category', ['class'=>'btn btn-primary ']) !!}
+            <a href="{{ route('admin.categories.index') }}" class='btn btn-default '>Back</a>
+        </div>
     {!! Form::close() !!}
 
 @endsection
--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -10,19 +10,12 @@
         </ul>
     @endif
 
-    {!! Form::open(['route'=>['admin.categories.update', $category->id], 'method' => 'put']) !!}
-    <div class="form-group">
-        {!! Form::label('name', 'Name:') !!}
-        {!! Form::text('name', $category->name, ['class'=>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('description', 'Description:') !!}
-        {!! Form::textarea('description', $category->description, ['class'=>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::submit('Save Category', ['class'=>'btn btn-primary']) !!}
-        <a href="{{ route('admin.categories.index') }}" class='btn btn-default '>Back</a>
-    </div>
+    {!! Form::model($category, ['route'=>['admin.categories.update', $category->id], 'method' => 'put']) !!}
+        @include('categories._form')
+        <div class="form-group">
+            {!! Form::submit('Save Category', ['class'=>'btn btn-primary']) !!}
+            <a href="{{ route('admin.categories.index') }}" class='btn btn-default '>Back</a>
+        </div>
     {!! Form::close() !!}
 
 @endsection
